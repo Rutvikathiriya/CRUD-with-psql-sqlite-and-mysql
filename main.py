189da80dc94c4ac79cd5c05b6c2039d51ebf6223
Rutvi Kathiriya <rutvi.kathiriya@drcsystems.com>
2017-07-04 15:50:00 +0530

    Third commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import sys
 import db_query 
 
+def create_table():
+    data.create()
+
 def insert():
     id = raw_input("Enter id : ")
     name = raw_input("Enter name : ")
@@ -28,6 +31,10 @@ def del_col():
     col = raw_input("Enter a column to del : ")
     data.deletecol(col)
 
+def delete_table():
+    delcol = raw_input("Enter table name to delete : ")
+    data.delete_table1(delcol)
+
 def list_table():
     print "1.sqlite\n2.Mysql\n3.postgresql\n"
     user = raw_input("Enter database : ")
@@ -39,39 +46,76 @@ def list_table():
         data.list_table_postgresql()
 
 
+def list_database():
+    print "1.sqlite\n2.Mysql\n3.postgresql\n"
+    user = raw_input("Enter database : ")
+    if user == '1':
+        data.list_db_sqlite()
+    elif user == '2':
+        data.list_db_mysql()
+    elif user == '3':
+        data.list_db_postgresql()
+
+def create_db():
+    print "1.sqlite\n2.Mysql\n3.postgresql\n"
+    user = raw_input("Enter database : ")
+    if user == '1':
+        data.create_db_sqlite()
+    elif user == '2':
+        data.create_db_mysql()
+    elif user == '3':
+        data.create_db_postgresql()
+
 
 def opration():
-    print "1.Insert "
-    print "2.Read data "
-    print "3.Update "
-    print "4.Delete "
-    print "5.Add coloumn"
-    print "6.Delete coloumn"
-    print "7.List Table"
-    print "8.Quit"
+    print "1.Create Databse"
+    print "2.List Of Table"
+    print "3.List Of Database"
+    print "4 Create Table"
+    print "5.Read Data"
+    print "6.Insert Data"
+    print "7.Update Data"
+    print "8.Delete Data"
+    print "9.Add coloumn"
+    print "10.Delete coloumn"
+    print "11.Delete Table"
+    print "12.Quit"
     user_opr = raw_input("Which operations you want to perform : ")
+    
     if user_opr == "1":
-        insert()
+        create_db()
         opration()
     elif user_opr == "2":
-        data.retrive()
-        opration()
-    elif user_opr == "3":
-        update()
-        opration()
-    elif user_opr == "4":
-        delete()
-        opration()
-    elif user_opr == "5":
-        add_col()
-        opration()
-    elif user_opr == "6":
-        del_col()
-        opration()
-    elif user_opr == "7":
         list_table()
         opration()
+    elif user_opr == "3":
+        list_database()
+        opration()
+    elif user_opr == "4":
+        create_table()
+        opration()
+    elif user_opr == "5":
+        data.retrive()
+        opration()
+    elif user_opr == "6":
+        insert()
+        opration()
+    elif user_opr == "7":
+        update()
+        opration()
     elif user_opr == "8":
+        delete()
+        opration()
+    elif user_opr == "9":
+        add_col()
+        opration()
+    elif user_opr == "10":
+        del_col()
+        opration()
+    elif user_opr == "11":
+        delete_table()
+        opration()
+    elif user_opr == "12":
         quit()
 
 
@@ -91,7 +135,5 @@ def choice():
         data = db_query.DataBase(selectDB)
         data.create()
         opration()
-
-
 
 choice()
