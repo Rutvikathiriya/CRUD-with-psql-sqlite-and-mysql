62e42a5e8ece71c3fcf544aff645225ac62dcb35
Rutvi Kathiriya <rutvi.kathiriya@drcsystems.com>
2017-07-05 13:05:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,23 @@
 import sys
 import db_query 
 
+def create_table_static():
+    data.create_tab()
+
 def create_table():
-    data.create()
+        cnt = int(raw_input("Enter a no of col : "))
+        i = 0
+        if i == cnt:
+            exit()
+        else:
+            str1 = ""
+            for i in range(cnt):
+                col1 = raw_input("Enter a col name : ")
+                type_col = raw_input("Enter a type of col : ")
+                str1 = str1 + str(col1) + " " + str(type_col) + ", "
+        str1 = str1[:-2]
+        #print str1
+        data.create(str1)
 
 def insert():
     id = raw_input("Enter id : ")
@@ -68,55 +83,60 @@ def create_db():
 
 
 def opration():
-    print "1.Create Databse"
-    print "2.List Of Table"
-    print "3.List Of Database"
-    print "4 Create Table"
-    print "5.Read Data"
-    print "6.Insert Data"
-    print "7.Update Data"
-    print "8.Delete Data"
-    print "9.Add coloumn"
-    print "10.Delete coloumn"
-    print "11.Delete Table"
-    print "12.Quit"
+    print "1.Create Table from user"
+    print "2.Create Databse"
+    print "3.List Of Table"
+    print "4.List Of Database"
+    print "5.Crete Table"
+    print "6.Read Data"
+    print "7.Insert Data"
+    print "8.Update Data"
+    print "9.Delete Data"
+    print "10.Add coloumn"
+    print "11.Delete coloumn"
+    print "12.Delete Table"
+    print "13.Quit"
+
     user_opr = raw_input("Which operations you want to perform : ")
-    
     if user_opr == "1":
-        create_db()
-        opration()
-    elif user_opr == "2":
-        list_table()
-        opration()
-    elif user_opr == "3":
-        list_database()
-        opration()
-    elif user_opr == "4":
         create_table()
         opration()
+    elif user_opr == "2":
+        create_db()
+        opration()
+    elif user_opr == "3":
+        list_table()
+        opration()
+    elif user_opr == "4":
+        list_database()
+        opration()
     elif user_opr == "5":
-        data.retrive()
+        create_table_static()
         opration()
     elif user_opr == "6":
-        insert()
+        data.retrive()
         opration()
     elif user_opr == "7":
-        update()
+        insert()
         opration()
     elif user_opr == "8":
-        delete()
+        update()
         opration()
     elif user_opr == "9":
-        add_col()
+        delete()
         opration()
     elif user_opr == "10":
-        del_col()
+        add_col()
         opration()
     elif user_opr == "11":
-        delete_table()
+        del_col()
         opration()
     elif user_opr == "12":
+        delete_table()
+        opration()
+    elif user_opr == "13":
         quit()
+
 
 
 def choice():
@@ -128,12 +148,14 @@ def choice():
     print "---------------"
     selectDB = raw_input("Choose Database:")
 
+
     if selectDB == '4':
         exit()
     elif selectDB:
         global data
-        data = db_query.DataBase(selectDB)
-        data.create()
+        global tab_name
+        tab_name = raw_input("enter a table name :")
+        data = db_query.DataBase(tab_name,selectDB)
         opration()
 
 choice()
